8c53b4daf0956f4c575fa1aca04e6c14f37fc7b3
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2023-02-27 09:04:31 +0100

    Add files via upload

---
+++ index000_Jupyter.py
@@ -0,0 +1,20 @@
+"""
+Jupyter
+
+Jupyter est un notebook open source qui permet de faciliter la visualisation
+des données qui se fait de manière classique dans une console dans des
+éditeurs de texte et IDE du stye SublimeText, VisualStudioCode, Pycharm,
+notamment pour le domaine de la data science
+
+Modalités d'installation et d'activation de Jupyter :
+-> Création de l'environnement virtuel : python -m venv virt
+-> Activation de l'environnement virtuel : source virt/Scripts/activate
+-> Mise à jour de Python : -m pip install --upgrade pip
+-> Installation de jupyter : python -m pip install jupyter
+-> Activitation de jypyter notebook : jupyter notebook
+-> Quitter jupyter notebook dans la console : CTRL + C
+
+Éditeur : Laurent REYNAUD
+Date : 30-0-2021
+"""
+
