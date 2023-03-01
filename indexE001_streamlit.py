5db2c3fd2b2125f9796d95acaa1a04bb8b95f3cf
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2023-03-01 14:32:42 +0100

    Add files via upload

---
+++ indexE001_streamlit.py
@@ -0,0 +1,19 @@
+"""
+Lien : https://www.youtube.com/watch?v=HNoF9dU_vrU&list=PLmJWMf9F8euQKADN-mSCpTlp7uYDyCQNF&index=3
+Cours : Formation Streamlit : Création d'une application Hello World dans Streamlit (n°3)
+
+Activer l'application web : 
+dans la console saisir streamlit run nomfichier.py
+
+Pour arrêter l'application web :
+dans la console -> touches CTRL + C
+
+Accéder à la documentation sur streamlit : 
+dans la console saisir streamlit hello
+
+Date : 01-03-23
+"""
+
+import streamlit as st
+
+st.write('Hello World! My name is John Gerald 😎')
