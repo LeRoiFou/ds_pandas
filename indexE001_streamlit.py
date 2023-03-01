a8cb48a5e32a9db1d18f74d39ca4bec43d5a9fa5
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2023-03-01 18:34:42 +0100

    Update indexE001_streamlit.py

--- indexE001_streamlit.py
+++ indexE001_streamlit.py
@@ -11,6 +11,15 @@ dans la console -> touches CTRL + C
 Accéder à la documentation sur streamlit : 
 dans la console saisir streamlit hello
 
+Pour partager le fichier sur @ :
+https://share.streamlit.io/
+Puis récupérer le fichier .py mis sur GitHub
+Dans le cas où l'application ne se lancerait pas, cela peut provenir de packages
+non reconnus dans streamlit, tel que matplotlib.
+Pour cela, crééer dans la même répertoire que le fichier .py un fichier .txt 
+avec le nom "requirements" : dans ce mettre le package à recourir ainsi que sa version
+Exemple : matplotlib==3.6.3
+
 Date : 01-03-23
 """
 
